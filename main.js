af41b450a2e629975d264fe2f815a89dd007f825
Cole <hoopwalker@yahoo.com>
2023-06-26 19:32:54 -0700

    important commands to main .js

--- main.js
+++ main.js
@@ -1,11 +1,8 @@
-import "dotenv/config"
-
-console.log(process.env.TOKEN);
-// Require the necessary discord.js classes
-import {Client, Events, GatewayIntentBits} from "discord.js";
-// const { token } = require('./config.json');
+require('dotenv').config();
+const {Client, Events, GatewayIntentBits, Collection} = require("discord.js");
+const fs = require("fs")
+const path = require("path")
 const token = process.env.TOKEN
-
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
@@ -15,5 +12,18 @@ client.once(Events.ClientReady, c => {
 	console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
+// import our commands
+client.commands = new Collection();
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	if ('data' in command && 'execute' in command) {
+		client.commands.set(command.data.name, command);
+	} else {
+		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+	}
+}
 // Log in to Discord with your client's token
 client.login(token);
